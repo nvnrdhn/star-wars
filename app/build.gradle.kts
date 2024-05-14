993b819c6575eeb7b4cff1c89c63952aeda28d40
Novan Ardhana <novan.ardhana@traveloka.com>
2024-05-14 21:50:42 +0700

    add dependencies and rule for unit test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,4 +90,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    testImplementation(libs.mockk)
+    testImplementation(libs.coroutines.test)
 }
